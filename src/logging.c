e39a74fec481645bbc8af588fd702a9cef42db01
Darius Marian <marian.darius98@gmail.com>
2020-11-21 18:42:26 +0200

    Refactor logging.

--- src/logging.c
+++ src/logging.c
@@ -1,30 +1,43 @@
 #include "logging.h"
 
 #include <pthread.h>
+#include <stdarg.h>
 #include <time.h>
 
-static pthread_mutexattr_t logging_mutex_attr;
 static pthread_mutex_t logging_mutex;
 
 void init_logging() {
-    pthread_mutexattr_init(&logging_mutex_attr);
-    pthread_mutex_init(&logging_mutex, &logging_mutex_attr);
+    int err = pthread_mutex_init(&logging_mutex, 0);
+    if (err != 0) {
+        fprintf(stderr, "Failed to initialize logging mutex: pthread_mutex_init() error=%d\n", err);
+        abort();
+    }
 }
 
-void acquire_log_mutex() {
-    pthread_mutex_lock(&logging_mutex);
-}
-
-void log_formatted_time() {
+void internal_log_message(const char* filename, int lineno, const char* level, const char* fmt, ...) {
     // Format is [YYYY-MM-DD HH:mm:ss], of length 21
-    char buffer[22];
+    char tm_buffer[22];
     time_t timestamp;
     time(&timestamp);
-    strftime(buffer, 21, "[%F %T]", gmtime(&timestamp));
-    buffer[21] = 0;
-    fprintf(stderr, "%s", buffer);
-}
 
-void release_log_mutex() {
-    pthread_mutex_unlock(&logging_mutex);
+    int err = pthread_mutex_lock(&logging_mutex);
+    if (err != 0) {
+        fprintf(stderr, "Failed to lock logging mutex: pthread_mutex_lock() error=%d\n", err);
+        abort();
+    }
+    strftime(tm_buffer, 21, "[%F %T]", gmtime(&timestamp));
+    tm_buffer[21] = 0;
+    fprintf(stderr, "%s %s ", tm_buffer, level);
+    if (filename != 0) {
+        fprintf(stderr, "(%s:%d) ", filename, lineno);
+    }
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    fprintf(stderr, "\n");
+    err = pthread_mutex_unlock(&logging_mutex);
+    if (err != 0) {
+        fprintf(stderr, "Failed to unlock logging mutex: pthread_mutex_unlock() error=%d\n", err);
+        abort();
+    }
 }
--- src/logging.h
+++ src/logging.h
@@ -6,11 +6,8 @@
 
 void init_logging();
 
-void acquire_log_mutex();
-
-void log_formatted_time();
-
-void release_log_mutex();
+void internal_log_message(const char* filename, int lineno, const char* level, const char* fmt, ...)
+    __attribute__((format(printf, 4, 5)));
 
 // Default logging level is info. Logging level is only controlled at build time.
 #ifndef NDC_LOG_LEVEL
@@ -19,52 +16,31 @@ void release_log_mutex();
 
 // By default, log messages do not contain file name and line number. This is only controlled at build time.
 #ifdef NDC_LOG_FILE_AND_LINE
-#define INTERNAL_LOG_FILE_AND_LINE_() fprintf(stderr, " (%s:%d) ", __FILE__, __LINE__)
+#define INTERNAL_LOG_(level, ...) internal_log_message(__FILE__, __LINE__, level, __VA_ARGS__)
 #else
-#define INTERNAL_LOG_FILE_AND_LINE_() ((void)0)
+#define INTERNAL_LOG_(level, ...) internal_log_message(0, 0, level, __ARGS__)
 #endif
 
-#define INTERNAL_LOG_(level, ...)  \
-    acquire_log_mutex();           \
-    log_formatted_time();          \
-    fprintf(stderr, " " level);    \
-    INTERNAL_LOG_FILE_AND_LINE_(); \
-    fprintf(stderr, __VA_ARGS__);  \
-    fprintf(stderr, "\n");         \
-    release_log_mutex()
-
 #if NDC_LOG_LEVEL <= 0
-#define LOG_DEBUG(...)                   \
-    do {                                 \
-        INTERNAL_LOG_("D", __VA_ARGS__); \
-    } while (0)
+#define LOG_DEBUG(...) INTERNAL_LOG_("D", __VA_ARGS__)
 #else
 #define LOG_DEBUG(...) ((void)0)
 #endif
 
 #if NDC_LOG_LEVEL <= 1
-#define LOG_INFO(...)                    \
-    do {                                 \
-        INTERNAL_LOG_("I", __VA_ARGS__); \
-    } while (0)
+#define LOG_INFO(...) INTERNAL_LOG_("I", __VA_ARGS__)
 #else
 #define LOG_INFO(...) ((void)0)
 #endif
 
 #if NDC_LOG_LEVEL <= 2
-#define LOG_WARN(...)                    \
-    do {                                 \
-        INTERNAL_LOG_("W", __VA_ARGS__); \
-    } while (0)
+#define LOG_WARN(...) INTERNAL_LOG_("W", __VA_ARGS__)
 #else
 #define LOG_WARN(...) ((void)0)
 #endif
 
 #if NDC_LOG_LEVEL <= 3
-#define LOG_ERROR(...)                   \
-    do {                                 \
-        INTERNAL_LOG_("E", __VA_ARGS__); \
-    } while (0)
+#define LOG_ERROR(...) INTERNAL_LOG_("E", __VA_ARGS__)
 #else
 #define LOG_ERROR(...) ((void)0)
 #endif
