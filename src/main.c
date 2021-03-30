923b86e8a5c9d43484cbffe05e7bde1a0780eb90
Darius Marian <marian.darius98@gmail.com>
2021-03-30 17:14:12 +0300

    Split build into separate static libraries.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,32 @@
 #include <signal.h>
 
-#include "app.h"
 #include "conf/conf.h"
+#include "http/handlers/static_files/file_cache.h"
+#include "http/handlers/static_files/static_files_handler.h"
+#include "http/server/http_server.h"
 #include "logging/logging.h"
+
+static int always_should_handle(UNUSED void* data, UNUSED struct http_req* req) {
+    return 1;
+}
+
+static void run_ndc_application_sync(const struct conf* conf) {
+    struct http_server http_server;
+    init_http_server(&http_server, conf);
+
+    struct file_cache file_cache;
+    init_file_cache(&file_cache, &conf->file_cache);
+    struct static_files_handler static_files_handler;
+    init_static_file_server(&static_files_handler, &file_cache, "./");
+    struct http_handler handler;
+    handler.name = "static_files_handler";
+    handler.data = &static_files_handler;
+    handler.should_handle = always_should_handle;
+    handler.handle = static_file_server_handle;
+    install_http_handler(&http_server, handler);
+
+    start_http_server(&http_server);
+}
 
 int main() {
     // Ignore sigpipe, since we handle broken pipe errors in code.
